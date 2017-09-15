d760388fd46143e398a45438cdba6e9735350d92
ThierryO <Thierry.Onkelinx@inbo.be>
2017-09-15 09:59:10 +0200

    add batPulse-class

---
+++ R/batPulse-class.R
@@ -0,0 +1,33 @@
+#' The batPulse class
+#'
+#' Holds the relevant content of wav files
+#' @section Slots:
+#'   \describe{
+#'    \item{\code{Recording}}{The metadata on the recording}
+#'    \item{\code{SpecGram}}{The actual spectrogram}
+#'   }
+#' @name batPulse-class
+#' @rdname batPulse-class
+#' @exportClass batPulse
+#' @aliases batPulse-class
+#' @importFrom methods setClass
+#' @importClassesFrom sp SpatialPolygonsDataFrame
+#' @docType class
+#' @include batPulseMeta-class.R
+setClass(
+  "batPulse",
+  representation = representation(
+    Contour = "SpatialPolygonsDataFrame"
+  ),
+  contains = "batPulseMeta"
+)
+
+#' @importFrom methods setValidity
+#' @importFrom assertthat assert_that is.count is.string is.flag noNA
+#' @importFrom digest sha1
+setValidity(
+  "batPulse",
+  function(object){
+    return(TRUE)
+  }
+)
