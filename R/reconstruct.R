bf97c3e9ff4237293af4e796bf58ca3b7df4c559
ThierryO <thierry.onkelinx@inbo.be>
2017-12-30 00:07:21 +0100

    add reconstruct()

---
+++ R/reconstruct.R
@@ -0,0 +1,51 @@
+#' Reconstruct pulse from their unscales Fourier parameters
+#' @param params the unscales Fourier parameters
+#' @param n the number of point in each pulse
+#' @importFrom assertthat assert_that is.count has_name
+#' @importFrom dplyr %>% select bind_cols
+#' @importFrom tidyr gather
+#' @importFrom stats model.matrix
+#' @export
+reconstruct <- function(params, n = 100) {
+  assert_that(is.count(n))
+
+  suppressWarnings(
+    n.harmonic <- gsub(
+        "^h([[:digit:]]*)(sin|cos)_(time|frequency)$",
+        "\\1",
+        colnames(params)
+      ) %>%
+      as.integer() %>%
+      max(na.rm = TRUE)
+  )
+  z <- seq_len(n.harmonic) %>%
+    sprintf(fmt = "I(cospi(%1$i * ID)) + I(sinpi(%1$i * ID))") %>%
+    paste(collapse = "+") %>%
+    sprintf(fmt = "~%s") %>%
+    as.formula() %>%
+    model.matrix(data.frame(ID = 2 * (0:n) / n))
+
+  time <- params[, "d_time"] %>%
+    cbind(params[, grep("^h[[:digit:]]+(sin|cos)_time", colnames(params))]) %>%
+    as.matrix() %>%
+    tcrossprod(z) %>%
+    as.data.frame()
+  frequency <- params[, "d_frequency"] %>%
+    "+"(params[, "peak_frequency"]) %>%
+    cbind(
+      params[, grep("^h[[:digit:]]+(sin|cos)_frequency", colnames(params))]
+    ) %>%
+    as.matrix() %>%
+    tcrossprod(z) %>%
+    as.data.frame()
+  if (has_name(params, "pulse")) {
+    time$pulse <- frequency$pulse <- params[, "pulse"]
+  } else {
+    time$pulse <- frequency$pulse <- rownames(params)
+  }
+  gather(time, "order", "time", seq_len(n + 1)) %>%
+    bind_cols(
+      gather(frequency, "order", "frequency", seq_len(n + 1)) %>%
+        select("frequency")
+    )
+}
