286192b544cdb26bef5010b1f66c089e0c490864
ThierryO <thierry.onkelinx@inbo.be>
2017-12-31 00:32:24 +0100

    bat_som() gains a ... argument

--- R/bat_som.R
+++ R/bat_som.R
@@ -1,18 +1,23 @@
 #' Calculate a self organizing map on bat pulse features
-#' @param x the output of \code{db2ml()}
+#' @param x the output of \code{\link{db2ml}}
 #' @param dims dimensions of the grid
-#' @param topo hexagonal or rectangular topology
+#' @param topo rectangular or hexagonal topology
+#' @param ... arguments past to \code{\link[kohonen]{som}}
 #' @export
 #' @importFrom kohonen som somgrid
 bat_som <- function(
   x,
   dims = rep(floor(log(nrow(x))), 2),
-  topo = c("hexagonal", "rectangular")
+  topo = c("rectangular", "hexagonal"),
+  ...
 ) {
   topo <- match.arg(topo)
+  xm <- as.matrix(x[, colnames(x) != "pulse"])
+  rownames(xm) <- x[, "pulse", drop = TRUE]
   x_som <- som(
-    as.matrix(x[, colnames(x) != "pulse"]),
-    grid = somgrid(xdim = dims[1], ydim = dims[2], topo = topo)
+    xm,
+    grid = somgrid(xdim = dims[1], ydim = dims[2], topo = topo),
+    ...
   )
   attr(x_som$codes[[1]], "maxL1") <- attr(x, "maxL1")
   attr(x_som$codes[[1]], "peak_frequency") <- attr(x, "peak_frequency")
