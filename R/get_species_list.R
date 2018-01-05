4b18e5bd722d3dac73480bf639bce5855312816a
ThierryO <thierry.onkelinx@inbo.be>
2018-01-05 15:05:01 +0100

    add get_species_list()

---
+++ R/get_species_list.R
@@ -0,0 +1,24 @@
+#' get the species list from the database
+#' @inherit db2sp
+#' @export
+#' @importFrom RSQLite dbGetQuery
+get_species_list <- function(connection) {
+  dbGetQuery(
+    conn = connection, "
+    WITH cte_parent AS (
+        SELECT id, parent, 0 AS level, abbreviation AS rank
+        FROM species
+        WHERE parent IS NULL
+      UNION
+        SELECT
+          s.id, s.parent, c.level + 1 AS level, c.rank || s.abbreviation AS rank
+        FROM species AS s INNER JOIN cte_parent AS c ON s.parent = c.id
+    )
+
+    SELECT
+      s.id, s.parent, s.description, s.abbreviation, c.level
+    FROM cte_parent AS c INNER JOIN species AS s ON c.id = s.id
+    ORDER BY c.rank
+    "
+  )
+}
